58aff4f1a9756d19f2ecb3c886e461f33cbb3cb4
chinmaya2020mt93611 <2020mt93611@wilp.bits-pilani.ac.in>
2022-04-10 18:24:56 +0530

    second commit

--- src/main/java/com/scalable/assignment/AssignmentController.java
+++ src/main/java/com/scalable/assignment/AssignmentController.java
@@ -1,22 +1,66 @@
 package com.scalable.assignment;
 
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
+
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class AssignmentController {
+	
+	@Autowired
+    private JdbcTemplate jdbcTemplate;
 
 	@GetMapping("/getDetails")
 	public String index() {
-		return "Scalable Assignment!";
+		String sql = "SELECT * FROM PATIENT";
+        
+		List<Map<String, Object>> employees = jdbcTemplate.queryForList(sql);
+		StringBuilder sb = new StringBuilder();
+		
+		if (employees!=null && !employees.isEmpty()) {
+			
+			for (Map<String, Object> employee : employees) {
+				
+				for (Iterator<Map.Entry<String, Object>> it = employee.entrySet().iterator(); it.hasNext();) {
+					Map.Entry<String, Object> entry = it.next();
+					String key = entry.getKey();
+					Object value = entry.getValue();
+					sb.append(key + " = " + value + "<br>");
+				}
+				sb.append("<br>");
+				sb.append("------------------------------");
+				sb.append("<br>");
+			}
+			
+		}
+		return sb.toString();
 	}
+	
+	//CREATE TABLE COMPANY(PatientId INT PRIMARY KEY NOT NULL, NAME TEXT NOT NULL, AGE TEXT NOT NULL);
+	
+	/*
+	 * @GetMapping("/createTable") public void tableCreate() { String sql =
+	 * "CREATE TABLE PATIENT(PatientId INT PRIMARY KEY NOT NULL, NAME TEXT NOT NULL, AGE TEXT NOT NULL)"
+	 * ;
+	 * 
+	 * jdbcTemplate.execute(sql); }
+	 */
 	
 	// API: POST - /add/patientDetails
 	@PostMapping("/add/patientDetails")
-	public void addPatient(String patientid) {
-		
+	public void addPatient(@RequestBody PatientRecord request, HttpServletResponse response) {
+		String sql = "INSERT INTO PATIENT VALUES (?::int, ?, ?)";
+		jdbcTemplate.update(sql, request.patientId, request.name, request.age);
 	}
 	
 	@PutMapping("/update/patientDetails")
@@ -26,7 +70,6 @@ public class AssignmentController {
 	
 	@PostMapping("/fetch/patientDrugDetails")
 	public void fetchDrug(String patientid) {
-		//API that Komal will build
 		
 	}
 
